b479fdf94af08cc2555f076ec0f08dfbc74f969c
Hector Martinez <hdmartinez@pol.una.py>
2015-04-27 09:15:27 -0400

    Alias para test de inet y funcion para svn diff
    Agrega un alias para realizar ping al servidor DNS de google, de modo a
    verificar si existe conexion a internet. Tambien agrega una funcion para
    visualizar las diferencias de un archivo utilizando "less"

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -6,3 +6,9 @@ alias ss="svn status"
 alias sd="svn diff | less"
 alias sl="svn log | less"
 alias psg="ps aux | grep"
+alias testnet="ping 8.8.8.8"
+
+
+function svndiff {
+    svn diff $1 | less
+}  
